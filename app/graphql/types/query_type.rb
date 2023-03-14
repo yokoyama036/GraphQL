230d394265d764c0dd42a43e7ff03827e84a2c23
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-03-14 10:48:12 +0900

    init

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :blog, BlogType, null: false,
+    def blog
+      Blog.last
+      # context[:blog]
+    end
+  end
+end
